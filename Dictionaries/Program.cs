9d61b3eae0e5d743760af6b08dcb96d6259e623b
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-26 15:05:03 -0300

    Added the Use() method

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -8,35 +8,43 @@ namespace Dictionaries
 {
     internal class Program
     {
-        static Dictionary<string, string> itemDescriptions = new Dictionary<string, string>();
+        static Dictionary<string, string> inventory = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
             FillInventory();
             ShowInventory();
             Describe("Phone");
+            Use("Phone");
             Describe("A");
             Describe("Book");
+            Use("Book");
+            Describe("Apple Juice");
+            Drop("Apple Juice");
+            Describe("Apple");
+            Use("Apple");
             Describe("Key");
             Describe("Letter");
+            Use("Letter");
+            Die();
         }
 
         static void FillInventory()
         {
-            itemDescriptions.Add("Phone", "This is yours.");
-            itemDescriptions.Add("Health Potion", "It tastes awful but it works");
-            itemDescriptions.Add("Apple", "Food for a health nut");
-            itemDescriptions.Add("Apple Juice", "Liquid apples");
-            itemDescriptions.Add("Book", "It's written in a language you don't know");
-            itemDescriptions.Add("Letter", "This doesn't belong to you, don't read it");
-            itemDescriptions.Add("Key", "Used to open locks");
+            inventory.Add("Phone", "This is yours.");
+            inventory.Add("Health Potion", "It tastes awful but it works");
+            inventory.Add("Apple", "Food for a health nut");
+            inventory.Add("Apple Juice", "Liquid apples");
+            inventory.Add("Book", "It's written in a language you don't know");
+            inventory.Add("Letter", "This doesn't belong to you, don't read it");
+            inventory.Add("Key", "Opens locks, but there is no lock");
         }
 
         static void ShowInventory()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Inventory (" + itemDescriptions.Count.ToString() + " items)");
-            foreach(string item in itemDescriptions.Keys)
+            Console.WriteLine("Inventory (" + inventory.Count.ToString() + " items)");
+            foreach(string item in inventory.Keys)
             {
                 Console.WriteLine("|" + item + "|");
             }
@@ -47,14 +55,64 @@ namespace Dictionaries
         static void Describe(string key)
         {
             Console.WriteLine("Attempting to describe " + key);
-            if (itemDescriptions.ContainsKey(key))
+            if (inventory.ContainsKey(key))
             {
-                Console.WriteLine(key + ":" + itemDescriptions[key]);
+                Console.WriteLine(key + ": " + inventory[key]);
             }
             else
             {
                 Console.WriteLine("Error: No " + key + "s in your inventory");
             }
+            ShowInventory();
+        }
+
+        static void Drop(string key)
+        {
+            Console.WriteLine("Attempting to drop " + key);
+            if (inventory.ContainsKey(key))
+            {
+                inventory.Remove(key);
+            }
+            else
+            {
+                Console.WriteLine("Error: No " + key + "s in your inventory");
+            }
+            ShowInventory();
+        }
+
+        static void Use(string key)
+        {
+            Console.WriteLine("Attempting to use " + key);
+            if (inventory.ContainsKey(key))
+            {
+                if(key == "Apple")
+                {
+                    Console.WriteLine("You ate it.");
+                    inventory.Remove(key);
+                }else if(key == "Book")
+                {
+                    Console.WriteLine("You still can't read this.");
+                }else if(key == "Letter")
+                {
+                    Console.WriteLine("The letter says: I really hope the player doesn't read this, that would be an invasion of privacy.");
+                }
+                else
+                {
+                    Console.WriteLine("You have no use for this at the moment.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error: No " + key + "s in your inventory");
+            }
+            ShowInventory();
+        }
+
+        static void Die()
+        {
+            Console.WriteLine("The player has died and lost all their stuff");
+            inventory.Clear();
             ShowInventory();
         }
 
